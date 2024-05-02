6d1fb80ca827c17bdf0514c8aea07eeff4aebb84
jackrileynin <jackrileynin@gmail.com>
2024-05-02 15:02:33 -0600

    refractored functionality to avoid confusion for keydown events

--- Border-radius Previewer/script.js
+++ Border-radius Previewer/script.js
@@ -1,71 +1,41 @@
 let intervalId;
+let keyState = {};
 
 document.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowUp" && e.key === "ArrowRight") {
-        clearInterval(intervalId); 
+    keyState[e.key] = true;
+    if (!intervalId) {
         intervalId = setInterval(function() {
-            let shape1 = document.getElementsByClassName('shape')[0]; 
-            let currentBorderRadius = parseInt(shape1.style.borderTopRightRadius || 0); 
-            let newBorderRadius = currentBorderRadius + 2; 
-            shape1.style.borderTopRightRadius = newBorderRadius + 'px'; 
-            console.log(`right corner radius:${currentBorderRadius}`)
-        }, 2);
-    }
-        if (e.key === "ArrowDown" && e.key === "ArrowLeft") {
-        clearInterval(intervalId); 
-        intervalId = setInterval(function() {
-            let shape1 = document.getElementsByClassName('shape')[0]; 
-            let currentBorderRadius = parseInt(shape1.style.borderBottomLeftRadius || 0);
-            let newBorderRadius = currentBorderRadius + 2; 
-            shape1.style.borderBottomLeftRadius = newBorderRadius + 'px'; 
-        }, 2);
-    }
-    
-    if (e.key === "ArrowUp" && e.key === "ArrowLeft"){
-        clearInterval(intervalId); 
-        intervalId = setInterval(function(){
             let shape1 = document.getElementsByClassName('shape')[0];
-            let currentBorderRadius = parseInt(shape1.style.borderTopLeftRadius || 0);
-            let newBorderRadius = currentBorderRadius + 2; 
-            shape1.style.borderTopLeftRadius = newBorderRadius + 'px'; 
-            console.log(`left corner radius:${currentBorderRadius}`)
-
-        }, 2)
+            if (keyState["ArrowRight"] && keyState["ArrowUp"]) {
+                let currentBorderRadius = parseInt(shape1.style.borderTopRightRadius || 0);
+                let newBorderRadius = currentBorderRadius + 2;
+                shape1.style.borderTopRightRadius = newBorderRadius + 'px';
+                console.log(`right corner radius:${currentBorderRadius}`)
+            }
+            if (keyState["ArrowDown"] && keyState["ArrowLeft"]) {
+                let currentBorderRadius = parseInt(shape1.style.borderBottomLeftRadius || 0);
+                let newBorderRadius = currentBorderRadius + 2;
+                shape1.style.borderBottomLeftRadius = newBorderRadius + 'px';
+            }
+            if (keyState["ArrowUp"] && keyState["ArrowLeft"]) {
+                let currentBorderRadius = parseInt(shape1.style.borderTopLeftRadius || 0);
+                let newBorderRadius = currentBorderRadius + 2;
+                shape1.style.borderTopLeftRadius = newBorderRadius + 'px';
+                console.log(`left corner radius:${currentBorderRadius}`)
+            }
+            if (keyState["ArrowDown"] && keyState["ArrowRight"]) {
+                let currentBorderRadius = parseInt(shape1.style.borderBottomRightRadius || 0);
+                let newBorderRadius = currentBorderRadius + 2;
+                shape1.style.borderBottomRightRadius = newBorderRadius + 'px';
+            }
+        }, 2);
     }
-    if(e.key === "ArrowDown" && e.key === "ArrowRight"){
+});
+
+document.addEventListener('keyup', function(e) {
+    keyState[e.key] = false;
+    if (e.key === "ArrowUp" || e.key === "ArrowRight" || e.key === "ArrowDown" || e.key === "ArrowLeft") {
         clearInterval(intervalId);
-        intervalId = setInterval(function(){
-            let shape1 = document.getElementsByClassName('shape')[0];
-            let currentBorderRadius = parseInt(shape1.style.borderBottomRightRadius || 0);
-            let newBorderRadius = currentBorderRadius + 2; 
-            shape1.style.borderTopRightRadius = newBorderRadius + 'px'; 
-        }, 2)
-    }
-
-
-
-});
-
-document.addEventListener('keyup', function(e) {
-    if (e.key === "ArrowUp" || e.key === "ArrowRight") {
-        clearInterval(intervalId); 
-    }
-});
-
-document.addEventListener('keyup', function(e) {
-    if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
-        clearInterval(intervalId); 
-    }
-});
-
-document.addEventListener('keyup', function(e) {
-    if (e.key === "ArrowDown" || e.key === "ArrowRight") {
-        clearInterval(intervalId); 
-    }
-});
-
-document.addEventListener('keyup', function(e) {
-    if (e.key === "ArrowDown" || e.key === "ArrowLeft") {
-        clearInterval(intervalId); 
+        intervalId = null;
     }
 });
