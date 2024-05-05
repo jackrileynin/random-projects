e0db1eb389b6f8a9af4506be562e2024714d8532
jackrileynin <jackrileynin@gmail.com>
2024-05-05 16:58:12 -0600

    added logic and css for user to input info

--- Count-Down-Timer/script.js
+++ Count-Down-Timer/script.js
@@ -2,6 +2,24 @@ const eventList = document.querySelector("ul");
 const createEvent = document.getElementById("create")
 let time = 0
 createEvent.addEventListener("click", function(){
-
+    eventList.classList.remove("fill-content")
+    const inputTitle = document.createElement("input")
+    inputTitle.type = "text"
+    inputTitle.placeholder = "Event Title"
+    const inputCountdown = document.createElement("input")
+    inputCountdown.type = "number"
+    inputCountdown.placeholder = "mm/dd/yyyy"
+    const inputContent = document.createElement("input")
+    inputContent.type = "text"
+    inputContent.placeholder = "Event Content"
+    const submit = document.createElement("button")
+    const editPlane = document.createElement("div");
+    editPlane.classList.add("edit-plane");
+    
+    editPlane.appendChild(inputTitle);
+    editPlane.appendChild(inputCountdown);
+    editPlane.appendChild(inputContent);
+    editPlane.appendChild(submit);
+    eventList.appendChild(editPlane);
 
 })
